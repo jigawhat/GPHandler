eb0efb0ff42d18d3932785a0b95472aff6afb8b1
Alfonso White <fonz@dyn1243-77.vpn.ic.ac.uk>
2016-01-05 23:08:26 +0000

    Testing using alpha variance instead of WhiteKernel

--- submit_some_gs_ids.py
+++ submit_some_gs_ids.py
@@ -1,6 +1,6 @@
 from GPRequester import submit_gp_request
 
-for aid in range(2500, 2700):
+for aid in range(2500, 2523):
     request = {
         "dataset": "landreg",
         "id": str(aid)
---
+++ submit_some_plot_reqs.py
@@ -0,0 +1,4 @@
+from PredictionPlotter import predict_and_plot_all
+
+for i in range(2500, 2523):
+    predict_and_plot_all("landreg", i, "")
