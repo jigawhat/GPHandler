Fixed plot size and experimenting with log adj price scaling factor

LandRegDataFormatter.py
@@ -53,7 +53,7 @@ class LandRegDataFormatter(DataFormatter):
         y = df['price'].values.ravel()
         self.price_scaling_factor = 0.3 / np.mean(y)
         if self.logadj:
-            self.price_scaling_factor *= 100
+            self.price_scaling_factor *= 5
         y = y * self.price_scaling_factor
         if self.logadj:
             y = np.vectorize(lambda x: math.log(x, self.logadj))(y)