ce8319044f7558e340ac66bd21a2f2a57d4589bd
Alfonso White <fonz@dyn1243-77.vpn.ic.ac.uk>
2016-01-06 17:01:47 +0000

    Fixed missing import on model tester

--- submit_model_test.py
+++ submit_model_test.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from DataLoader import DataLoader
 from GPRequester import submit_gp_request
+from PredictionPlotter import predict_and_plot_all
 
 aid = 10
 fn_suffix = "_logadj_test"
