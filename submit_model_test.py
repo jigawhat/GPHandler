30f7aa4333987dca81bc695c0963dbd37abc43fa
Alfonso White <fonz@dyn1243-77.vpn.ic.ac.uk>
2016-01-06 17:26:11 +0000

    Improved model tester

--- submit_model_test.py
+++ submit_model_test.py
@@ -1,11 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from DataLoader import DataLoader
+import sys
 from GPRequester import submit_gp_request
 from PredictionPlotter import predict_and_plot_all
 
-aid = 10
-fn_suffix = "_logadj_test"
+gp_req = int(float(sys.argv[1])) if(len(sys.argv) > 1) else 0
+aid = int(float(sys.argv[2])) if(len(sys.argv) > 2) else 10
+fn_suffix = sys.argv[3] if(len(sys.argv) > 3) else ""
 
 request = {
     'dataset': 'landreg',
@@ -15,6 +14,6 @@ request = {
         'filename_suffix': fn_suffix
     }
 }
-
-submit_gp_request(request)
+if gp_req:
+    submit_gp_request(request)
 predict_and_plot_all("landreg", aid, fn_suffix)
